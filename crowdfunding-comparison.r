594cbae88514a59f6ef6eccc6ad3f96a1f3ac5f4
Eric Minikel <ericminikel@gmail.com>
2014-10-18 06:09:28 -0400

    removed 'largest project'

--- crowdfunding-comparison.r
+++ crowdfunding-comparison.r
@@ -1,9 +1,8 @@
 
 pdf('no-substitute.pdf',width=8,height=6)
-qtys = c(17217,45629,300000,405874)
+qtys = c(17217,45629,405874)
 labels = c("our project [1]",
-           "average successful\ncrowdfunding\nproject [2]",
-           "largest successful\ncrowdfunding\nproject to date [2]",
+           "average successful\ncancer crowdfunding\nproject [2]",
            "average R01 grant [3]")
 citations = c("[1] https://experiment.com/projects/can-anle138b-delay-the-onset-of-genetic-prion-disease",
     "[2] Reviewed in Dragojlovic & Lind 2014 'Crowdfunding drug development: the state of play in oncology and rare diseases' PMID: 24973645",
@@ -11,12 +10,11 @@ citations = c("[1] https://experiment.com/projects/can-anle138b-delay-the-onset-
 
 k = '#CC1100'
 par(mar=c(7,1,3,1))
-plot(NA,NA,xlim=c(.5,4.5),ylim=c(0,500000),axes=FALSE,xlab='',ylab='',
+plot(NA,NA,xlim=c(.5,3.5),ylim=c(0,500000),axes=FALSE,xlab='',ylab='',
     main='Crowdfunding is no substitute for conventional funding sources')
-points(1:4,qtys,type='h',lend=1,lwd=50,col=k)
-text(1:4,qtys,labels=c("$17K","$45K","$300K","$406K"),pos=3)
+points(1:3,qtys,type='h',lend=1,lwd=100,col=k)
+text(1:3,qtys,labels=c("$17K","$45K","$406K"),pos=3)
 abline(h=0)
-axis(side=1,at=1:4,labels=labels,lwd=0,lwd.ticks=0,cex.axis=.8,padj=0)
-#axis(side=2,at=qtys,labels=paste("$",prettyNum(qtys,big.mark=",",scientific=FALSE),sep=""),las=1,cex.axis=.8,lwd=0,lwd.ticks=1)
+axis(side=1,at=1:3,labels=labels,lwd=0,lwd.ticks=0,cex.axis=.8,padj=0)
 mtext(side=1,line=3:5,text=citations,cex=.5,adj=0)
 dev.off()
