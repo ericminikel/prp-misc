af0417da0557949ca780a17ea7c2df49873d9cb7
Eric Minikel <ericminikel@gmail.com>
2014-11-03 12:58:12 -0500

    added second plot

--- expression-level-incubation-time.r
+++ expression-level-incubation-time.r
@@ -19,3 +19,12 @@ text(explevel,terminal,labels=mousename,pos=c(4,4,3,2,2),cex=.8)
 text(rep(max(explevel),2),c(min(terminal)+7,min(symptoms)-7),pos=4,labels=c('terminal illness','symptom onset'),col=c(tk,sk))
 axis(side=1,at=explevel,labels=c('.5','1','3.5','6.5','10'),lwd=0,lwd.ticks=1,cex.axis=.8)
 axis(side=2,at=(0:4)*100,labels=(0:4)*100,lwd=0,lwd.ticks=1,las=1,cex.axis=.8)
+
+# also plot symptomatic phase as proportion of overall course
+par(mar=c(5,6,3,1))
+plot(explevel,(terminal-symptoms)/terminal,type='l',col='#458B00',lwd=8,
+    xlab='PrPC expression level (fold wild-type)',
+    ylab='Symptomatic phase as percent\nof overall disease course',
+    main='More PrPC means relatively shorter symptomatic phase',axes=FALSE)
+axis(side=1,at=explevel,labels=c('.5','1','3.5','6.5','10'),lwd=0,lwd.ticks=1,cex.axis=.8)
+axis(side=2,at=(0:4)/10,labels=paste((0:4)*10,"%",sep=""),lwd=0,lwd.ticks=1,las=1,cex.axis=.8)
