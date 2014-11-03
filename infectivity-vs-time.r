068bd9741545b3c289ff9db1c2ca1c921cecc379
Eric Minikel <ericminikel@gmail.com>
2014-11-03 14:08:05 -0500

    data from prusiner 1982

---
+++ infectivity-vs-time.r
@@ -0,0 +1,3 @@
+days = c(0,1,50,70)
+logtiter = c(7,2,9,9)
+plot(days,logtiter,type='l',ylim=c(0,9),xlab='days post-inoculation',ylab='prion titer (log10 ID50 units)',pch=15,main='degradation and accumulation of prion infectivity in hamsters',lwd=8,col='#333333')
